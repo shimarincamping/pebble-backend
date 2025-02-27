e4ea73a6143f103f851fe8a2dd7d86ba13e08078
Brendan Ooi <brendanooi16@gmail.com>
2025-02-27 20:50:51 +0800

    added POST /rewards endpoint, email capability

--- src/routes/rewardRoutes.js
+++ src/routes/rewardRoutes.js
@@ -1,10 +1,14 @@
-const express = require('express');
+const express = require("express");
 const rewardRouter = express.Router();
 
-const rewardController = require('../controllers/rewardController');
+const rewardController = require("../controllers/rewardController");
 
 // Route definitions
 rewardRouter.get("/", rewardController.getAllRewards);
 
+// Tickets must exist before spinning the wheel
+rewardRouter.use("/sendReward", rewardController.assertTicketExists);
 
+// Sending rewards to user email
+rewardRouter.post("/sendReward", rewardController.addNewReward);
 module.exports = rewardRouter;
