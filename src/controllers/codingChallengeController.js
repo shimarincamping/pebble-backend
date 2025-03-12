27a23b8fe01bc62c69d1e567a279569367e6fa82
shimarincamping <93362779+shimarincamping@users.noreply.github.com>
2025-03-12 11:17:49 +0800

    Added Create, Update, Delete for Goals

--- src/controllers/codingChallengeController.js
+++ src/controllers/codingChallengeController.js
@@ -1,18 +1,12 @@
 const firestoreService = require("../services/firestoreService");
 const documentExistsMiddleware = require("../middlewares/documentExistsMiddleware");
 
-const {
-    documentObjectArrayReduce,
-    sortObjectsByNonNumericFieldValues,
-    CODING_CHALLENGE_DIFFICULTY_ORDER,
-} = require("../utils/dataManipulationUtils");
+const { documentObjectArrayReduce, sortObjectsByNonNumericFieldValues, CODING_CHALLENGE_DIFFICULTY_ORDER } = require("../utils/dataManipulationUtils");
 const { throwError } = require("../middlewares/errorMiddleware");
 
 exports.assertCodingChallengeExists = (req, res, next) => {
     documentExistsMiddleware.assertExists(
-        `codingChallenges/${req.codingChallengeID}`,
-        res,
-        next
+        `codingChallenges/${req.codingChallengeID}`, res, next
     );
 };
 
@@ -23,24 +17,16 @@ exports.getAllCodingChallengeData = (req, res, next) => {
         .firebaseReadAll(`codingChallenges`, next)
         .then((codingChallengesData) => {
             return res.status(200).send(
-                documentObjectArrayReduce(
-                    sortObjectsByNonNumericFieldValues(
-                        codingChallengesData.map((c) => {
-                            c.lastAnsweredQuestion =
-                                c.lastAnsweredQuestion[currentUserID] || -1;
-                            c.isQuizCompleted =
-                                c.lastAnsweredQuestion >=
-                                c.quizQuestions.length;
+                documentObjectArrayReduce(sortObjectsByNonNumericFieldValues(
+                    codingChallengesData.map((c) => {
+                        c.lastAnsweredQuestion = c.lastAnsweredQuestion[currentUserID] || -1;
+                        c.isQuizCompleted = c.lastAnsweredQuestion >= c.quizQuestions.length;
+                        const { quizQuestions, ...dataWithoutQuestions } = c;
 
-                            const { quizQuestions, ...dataWithoutQuestions } =
-                                c;
-
-                            return dataWithoutQuestions;
-                        }),
-                        CODING_CHALLENGE_DIFFICULTY_ORDER,
-                        "quizDifficulty"
-                    )
-                )
+                        return dataWithoutQuestions;
+                    }),
+                    CODING_CHALLENGE_DIFFICULTY_ORDER, "quizDifficulty"
+                ))
             );
         });
 };
@@ -55,71 +41,49 @@ exports.updateUserChallengeProgress = (req, res, next) => {
     if (req.body?.newProgressValue != null) {
         firestoreService
             .firebaseWrite(
-                `codingChallenges/${req.codingChallengeID}`,
-                {
-                    lastAnsweredQuestion: {
-                        [`${currentUserID}`]: req.body.newProgressValue,
-                    },
-                },
-                next
+                `codingChallenges/${req.codingChallengeID}`, {
+                    lastAnsweredQuestion: { [`${currentUserID}`]: req.body.newProgressValue }
+                }, next
             )
             .then(() => {
-                return res
-                    .status(200)
-                    .send(
-                        `Successfully updated progress for ${currentUserID} on challenge ${req.codingChallengeID} to ${req.body.newProgressValue}`
-                    );
+                return res.status(200).send(
+                    `Successfully updated progress for ${currentUserID} on challenge ${req.codingChallengeID} to ${req.body.newProgressValue}`
+                );
             });
     } else {
-        return throwError(
-            400,
-            `Missing expected value in request body: newProgressValue`,
-            next
-        );
+        return throwError(400, `Missing expected value in request body: newProgressValue`, next);
     }
 };
 
 exports.createNewCodingChallenge = (req, res, next) => {
-    const currentUserID = res.locals.currentUserID;
-
     if (req.body != null) {
-        firestoreService
-            .firebaseCreate(
-                `codingChallenges`,
-                {
-                    quizName: req.body.quizName || "New Quiz",
-                    quizDifficulty: req.body.quizDifficulty || "Easy",
-                    quizDescription:
-                        req.body.quizDescription || "Try the quiz now!",
-                    lastAnsweredQuestion: {},
-                    quizQuestions:
-                        req.body.quizQuestions?.map((question) => ({
-                            questionBody:
-                                question?.questionBody ||
-                                "Pick the correct answer",
-                            questionOptions:
-                                question?.questionOptions?.map((option) => ({
-                                    optionText: option?.optionText || "Option",
-                                    isCorrect: Boolean(option?.isCorrect),
-                                })) || {},
-                        })) || {},
-                },
-                next
-            )
-            .then(() => {
-                return res.status(200).send();
-            });
+        firestoreService.firebaseCreate(
+            `codingChallenges`, {
+                quizName: req.body.quizName || "New Quiz",
+                quizDifficulty: req.body.quizDifficulty || "Easy",
+                quizDescription: req.body.quizDescription || "Try the quiz now!",
+                lastAnsweredQuestion: {},
+                quizQuestions: req.body.quizQuestions?.map((question) => ({
+                    questionBody: question?.questionBody || "Pick the correct answer",
+                    questionOptions: question?.questionOptions?.map((option) => ({
+                        optionText: option?.optionText || "Option",
+                        isCorrect: Boolean(option?.isCorrect),
+                    })) || {}
+                })) || {}
+            }, next
+        ).then(() => {
+            return res.status(201).send();
+        });
     } else {
         return throwError(400, `Missing expected object in request body`, next);
     }
 };
 
 exports.deleteCodingChallenge = (req, res, next) => {
-    const currentUserID = res.locals.currentUserID;
-
-    firestoreService
-        .firebaseDelete(`codingChallenges/${req.codingChallengeID}`, next)
-        .then(() => {
-            return res.status(204).send();
-        });
+    firestoreService.firebaseDelete(
+        `codingChallenges/${req.codingChallengeID}`, 
+        next
+    ).then(() => {
+        return res.status(204).send();
+    });
 };
--- src/routes/goalRoutes.js
+++ src/routes/goalRoutes.js
@@ -5,6 +5,20 @@ const goalController = require("../controllers/goalController");
 
 const { checkPermission } = require("../middlewares/verifyRoleMiddleware");
 
+
+// Pre-processes all routes that contain an ID parameter
+goalRouter.param("id", (req, res, next, id) => {
+    req.goalID = id;
+    next();
+});
+
+// ID must exist to perform operations that involve an ID parameter
+goalRouter.use(
+    "/:id",
+    goalController.assertGoalExists
+);
+
+
 // Route definitions
 goalRouter.get(
     "/",
