7717d562468fce33f4e5abe053550c74577b7b1b
Anoop Singh <anoopsinghhs77@gmail.com>
2025-03-17 18:08:02 +0800

    progress with fixing cv (can you learn how to code plz)

--- src/services/geminiService.js
+++ src/services/geminiService.js
@@ -1,6 +1,8 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
-const API_Key = process.env.PUBLIC_geminiAPIKey;
+// const API_Key = process.env.PUBLIC_geminiAPIKey;
+const API_Key = process.env.geminiAPIKey;
+console.log(`API_Key: ${API_Key}`);
 
 const gemini = new GoogleGenerativeAI(API_Key);
 const gemini15Flash = gemini.getGenerativeModel({ model: "gemini-1.5-flash" });
